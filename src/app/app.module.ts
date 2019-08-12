33be6a0b52e118d6d462a840de34aee67724c756
asch <a.schaal@evs.com>
2019-08-12 15:23:28 +0200

    add material components and cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//All materials components imports are here !
+import { MatButtonModule } from "@angular/material/button"
+
+//Animations module activation for material components.
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
